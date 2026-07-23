c2dd2725eed7353d341b58999c2801989bfb3e21
chiragmalik2612 <chiragmalik2612@gmail.com>
2026-07-23 22:28:41 +0530

    logo removed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,17 +32,16 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           
-          {/* --- LOGO SECTION --- */}
-          <div className="flex-shrink-0 flex items-center">
+          
+          {/* <div className="flex-shrink-0 flex items-center">
             <Link href="/" className="flex items-center transition-opacity hover:opacity-80">
-              {/* Responsive logo image: Adjusts height based on screen size (h-12 on mobile, h-14 on desktop) */}
               <img 
                 src="/logobiopro.png" 
                 alt="Biomanufacturing Process Lab Logo" 
                 className="h-12 md:h-14 w-auto object-contain"
               />
             </Link>
-          </div>
+          </div> */}
 
           {/* --- DESKTOP NAVIGATION --- */}
           <div className="hidden md:flex flex-1 justify-center items-center space-x-1">
